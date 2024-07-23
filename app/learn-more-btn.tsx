55e70598b9a57065ec244998c79105520895cecc
fei guo <812773673@qq.com>
2024-07-23 20:39:11 +0800

    fix:some-style

--- app/learn-more-btn.tsx
+++ app/learn-more-btn.tsx
@@ -7,8 +7,10 @@ export function LearnMoreBtn() {
   const { blockPosition, handleMouseEnter, handleMouseLeave, handleMouseMove } = useBtnHoverPosition();
   return (
     <div onMouseMove={handleMouseMove} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="div-block-5">
-      <div className="text-block-4">Learn More About Powfi</div>
-      <img src="images/箭头右_arrow-right.svg" loading="lazy" width="16" height="16" alt="" className="image-3" />
+      <div style={{ zIndex: 1 }} className="text-block-4">
+        Learn More About Powfi
+      </div>
+      <img style={{ zIndex: 1 }} src="images/箭头右_arrow-right.svg" loading="lazy" width="16" height="16" alt="" className="image-3" />
       <div
         style={{
           left: blockPosition ? `${blockPosition}px` : 'auto',
